989ff8c1b0c6217c378c290d92e779ea9f4a4b54
Ian J <ianj67@comcast.net>
2024-11-10 14:12:18 -0700

    message

---
+++ data_manipulation.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+data = {
+    'rocket' : [
+        'Falcon 1',
+        'Falcon 9',
+        'Falcon Hevy',
+    ],
+    'launches' : [5, 100, 3],
+}
+
+df = pd.DataFrame(data)
+
+print(df)
+
+print(df['rocket'])
+
+falcon9_df = df[df['rocket'] == 'Falcon 9']
+
+df['success_rate'] = [0.4,0.98,1.0]
+
+print(data)
---
+++ variables.py
@@ -0,0 +1,8 @@
+#These are just random variables for testing, they dont mean much. 
+launch_count = 10
+success_rate = 0.95
+rocket_name = 'Falcon 9'
+is_active = True
+
+total_launches = launch_count +5
+print(total_launches)
